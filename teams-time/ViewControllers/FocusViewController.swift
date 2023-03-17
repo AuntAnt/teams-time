77cef175662d954234b6ff47e6fdd0c3dbb30a4c
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-03-17 14:28:22 +0500

    work with ui

---
+++ teams-time/ViewControllers/FocusViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FocusViewController.swift
+//  teams-time
+//
+//  Created by Bektemur Mamashayev on 17/03/23.
+//
+
+import UIKit
+
+class FocusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
