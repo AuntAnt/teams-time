b45ff5fbc708ed1896f679dee7e9cf25044568a6
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-03-18 11:20:24 +0500

    add timer functionality

--- teams-time/ViewControllers/FocusViewController.swift
+++ teams-time/ViewControllers/FocusViewController.swift
@@ -7,23 +7,64 @@
 
 import UIKit
 
-class FocusViewController: UIViewController {
-
+final class FocusViewController: UIViewController {
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var pauseButton: UIButton!
+    
+    
+    private var timeRemaining = 1 * 60
+    private var pauseRemaining = 0 * 60
+    private var timer: Timer!
+    private var timerStarted = false
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func startButtonPressed() {
+        if !timerStarted {
+            timer = Timer.scheduledTimer(
+                withTimeInterval: 1,
+                repeats: true,
+                block: { [weak self] timer in
+                    self?.updateTimer()
+                }
+            )
+        }
     }
-    */
+    
+    @IBAction func pauseButtonPressed() {
+        if timer != nil {
+            timer.invalidate()
+            startButton.setTitle("Resume", for: .normal)
+            timerStarted = false
+        }
+    }
+    
+}
 
+private extension FocusViewController {
+    func updateTimer() {
+        if timeRemaining > 0 {
+            
+            if !timerStarted {
+                timerStarted = true
+            }
+            
+            timeRemaining -= 1
+            let minutes = timeRemaining / 60
+            let seconds = timeRemaining % 60
+            let timeString = String(format: " %02d : %02d ", minutes, seconds)
+            timerLabel.text = timeString
+        } else {
+            timer.invalidate()
+            timer = nil
+            timerStarted = false
+        }
+        
+    }
 }
